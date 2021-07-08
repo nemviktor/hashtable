6ce7f7c7a139e897264f4318caeb51d3183d8501
nemviktor <wookie7373@gmail.com>
2021-07-08 16:07:00 +0200

    Implementing the hashtable functionalities put, get, remove. 1 Test is still failing.

--- src/main/java/com/codecool/hashtable/HashTable.java
+++ src/main/java/com/codecool/hashtable/HashTable.java
@@ -13,7 +13,13 @@ public class HashTable<K,V> {
     private List<List<Entry>> buckets;
 
     private int hashFunc(K key) {
-        return Objects.hash(key);
+        int hashCode;
+        if (key != null) {
+            hashCode = Objects.hash(key);
+        } else {
+            hashCode = 0;
+        }
+        return hashCode;
     }
 
     public HashTable() {
@@ -25,12 +31,9 @@ public class HashTable<K,V> {
         }
     }
 
-
     private int getBucketIndexForKey(K key) {
-        int index = hashFunc(key) % bucketsSize;
-        System.out.println(index);
+        int index = Math.abs(hashFunc(key)) % bucketsSize;
         return index;
-//        throw new RuntimeException("FIXME");
     }
 
     private List<Entry> getBucketAtIndex(int position) {
@@ -39,38 +42,63 @@ public class HashTable<K,V> {
     }
 
     private Entry findEntryInBucket(K key, List<Entry> bucket) {
-        throw new RuntimeException("FIXME");
+        for (int i = 0; i<bucket.size(); i++) {
+            if (bucket.get(i).getKey().equals(key)) {
+                return bucket.get(i);
+            }
+        }
+        return null;
     }
 
     public V get(K key) {
-        int index = hashFunc(key) % bucketsSize;
+        int index = 0;
+        if (key != null) {
+            index = getBucketIndexForKey(key);
+        }
         int linkedLength = getBucketAtIndex(index).size();
-        System.out.println(getBucketAtIndex(index).size());
+
         V value = null;
         for (int i=0;i<linkedLength;i++) {
-            System.out.println(getBucketAtIndex(index).get(i).getKey().toString());
-//            if (getBucketAtIndex(index).get(i).getKey() == key) {
-//                value = (V) getBucketAtIndex(index).get(i).getValue();
-//            }
+            if (getBucketAtIndex(index).get(i).getKey().equals(key)) {
+                value = (V) getBucketAtIndex(index).get(i).getValue();
+//                List<Entry> bucket = getBucketAtIndex(index);
+//                Entry current = findEntryInBucket(key, bucket);
+//                value = (V) current.getValue();
+            }
         }
-//        throw new RuntimeException("FIXME");
         return value;
     }
 
     public void put(K key, V value) {
-        int index = hashFunc(key) % bucketsSize;
-        List<Entry> toAdd = new LinkedList<>();
-        toAdd.add(new Entry(key, value));
-        buckets.set(index, toAdd);
-//        throw new RuntimeException("FIXME");
+        int index = getBucketIndexForKey(key);
+        List<Entry> bucket = getBucketAtIndex(index);
+
+        if (bucket == null) {
+            List<Entry> toAdd = new LinkedList<>();
+            toAdd.add(new Entry(key, value));
+            buckets.set(index, toAdd);
+        } else {
+            if (findEntryInBucket(key, bucket) == null) {
+                getBucketAtIndex(index).add(new Entry(key, value));
+            } else {
+                findEntryInBucket(key,bucket).setValue(value);
+            }
+        }
     }
 
     public V remove(K key) {
-        throw new RuntimeException("FIXME");
+        int index = getBucketIndexForKey(key);
+        List<Entry> current = getBucketAtIndex(index);
+        Entry toRemove = findEntryInBucket(key, current);
+        current.remove(toRemove);
+        return (V) toRemove.getValue();
     }
 
     public void clear() {
-        throw new RuntimeException("FIXME");
+//        buckets.clear();
+        for (int i =0; i<bucketsSize;i++) {
+            getBucketAtIndex(i).clear();
+        }
     }
 }
 
@@ -90,6 +118,14 @@ class Entry<K,V> {
 
     public K getKey() {
         return key;
+    }
+
+    public void setValue(V value){
+        this.value = value;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
     }
 
 }
